be62ac766bb334dce25a600bb71735beeaef7d60
samuelmorenov <uo266321@uniovi.es>
2019-12-14 14:07:25 +0100

    primera version funcional

--- Ejercicio4.py
+++ Ejercicio4.py
@@ -1,25 +1,18 @@
 #-------------------------------------------------------------------------------
-# Name:        Ejercicio1
-# Purpose:
-#
-# Author:      UO258774
-#
-# Created:     30/11/2019
-# Copyright:   (c) UO258774 2019
-# Licence:     <your licence>
+# Name:        Ejercicio4
 #-------------------------------------------------------------------------------
 
-import json # Para poder trabajar con objetos JSON
-from elasticsearch import Elasticsearch # Para poder trabajar con objetos Elasticsearch
+import json
+from elasticsearch import Elasticsearch
 
 def main():
 
     print("Ejercicio 4\n")
 
-    # Nos conectamos por defecto a localhost:9200
     es = Elasticsearch()
 
     numero_salidas = 5
+    query = "suicide suicidal kill myself killing myself end my life"
 
     results = es.search(
         index="reddit-mentalhealth",
@@ -28,7 +21,8 @@ def main():
             "query": {
                 "query_string": {
                     "default_field": "selftext",
-                    "query": "suicide",
+                    "query": query,
+                    "operator": "or"
                 }
             },
             "aggs": {
@@ -56,135 +50,17 @@ def main():
             }
     })
 
-
-
-"""
-
-##########################################################
-##########################################################
-##########################################################
     words = []
     for j in ["Subreddit", "Text", "Title"]:
-        [words.append(i["key"]) for i in results["aggregations"][j]["buckets"] if i["key"] not in words and any(i["key"] in s for s in data)]
-
-    print("--- Expresiones usadas ---")
-    for word in words:
-        print("\t",word)
-    print()
-##########################################################
-##########################################################
-##########################################################
-"""
-
-
-"""
-
-
-    # Objetos json (cada uno tendrá una key con el termino)
-    terminosSignificativos = results["aggregations"]["TerminosSignificativos"]["buckets"]
-
-    # Creamos un vector para almacenar los terminos
-    vectorTerminos = []
-
-    # Añadimos los términos
-    for i in terminosSignificativos:
-        vectorTerminos.append(i.get("key"))
-
-    #############################################################################
-    # Ya tenemos los téminos que están relaccionados con la palabra,
-    # ahora obtendremos los 10 post que más de esas palabras contengan.
-    #############################################################################
-
-    # 1. Obtener un string con las palabras
-    palabras=""
-    for i in vectorTerminos:
-        palabras+=i+" "
-
-    # 2. Hacer la consulta
-    results2=es.search(
-        index="reddit-mentalhealth",
-        body = {
-            "size":100,
-            "query": {
-                "match": {
-                    "selftext": {
-                        "query": palabras,
-                        "operator": "or"
-                    }
-                }
-            }
-        }
-    )
-
-    # Obtenemos los 100 primeros posts
-    posts=results2["hits"]["hits"]
-
-    # Ya tenemos los 100 primeros posts, pero sólo hace falta el autor,
-    # la fecha de creación y el texto. Hay que hacer un nuevo archivo
-    # json con los posts.
-
-    salida={}
-    salida['posts']=[]
-    for i in posts:
-
-        # Comprobamos que no haya un post igual, ya que hay post repetidos
-        # de gente que lo subio varias veces
-        yaEsta=False
-        for j in salida['posts']:
-            if j.get("Texto: ")==i["_source"].get("selftext"):
-                yaEsta=True
-
-        if yaEsta==False:
-            salida['posts'].append({
-                'Autor: ':i["_source"].get("author"),
-                'Fecha de creacion: ':i["_source"].get("created_utc"),
-                'Texto: ':i["_source"].get("selftext")
-            })
+        for i in results["aggregations"][j]["buckets"]:
+            if i["key"] not in words:
+                words.append(i["key"])
 
     # Guardar el archivo con formato json
-    with open('salida.json', 'w') as file:
-        json.dump(salida, file, indent=4)
+    with open('ejercicio4.json', 'w') as file:
+        json.dump(words, file, indent=4)
 
-    print("Se ha generado un archivo salida.json con los resultados")
-
-"""
-
-"""
-def config():
-    pp = pprint.PrettyPrinter(indent=2)
-
-    es = Elasticsearch()
-
-    #configuracion usada en el indice
-    argumentos = {
-      "properties": {
-        "author": {
-            "type": "text",
-            "term_vector": "yes",
-            "fielddata": "true"
-        },
-        "selftext": {
-            "type":"text",
-            "term_vector": "yes",
-            "fielddata": "true"
-        },
-        "title": {
-            "type":"text",
-            "term_vector": "yes",
-            "fielddata": "true"
-        },
-        "subreddit": {
-            "type":"text",
-            "term_vector": "yes",
-            "fielddata": "true"
-        }
-      }
-    }
-
-    es.indices.put_mapping(index="reddit-mentalhealth",doc_type="post",body=argumentos,ignore=400)
-
-    return es
-"""
+    print("Se ha generado un archivo ejercicio4.json con los resultados")
 
 if __name__ == '__main__':
     main()
